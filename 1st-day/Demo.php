0288d384138486aa497abede742ca8c0035dfdbb
aanal25patel <49318251+aanal25patel@users.noreply.github.com>
2021-05-30 23:31:23 +0530

    Add files via upload
    Day-1 
    Task: to create a small website using HTML, CSS

---
+++ 1st-day/Demo.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "This is PHP code block.<br>";
+echo "<h1>I am H1 tag in PHP code block.</h1>";
+
+?>
+
+<html>
+
+<body>
+Welcome to HTML code block.<br>
+<?php
+echo "I am PHP inside HTML block of code.";
+?>
+
+</body>
+
+
+
+
+</html>
---
+++ 1st-day/Variable_method.php
@@ -0,0 +1,25 @@
+<?php
+
+// variable
+
+$x = 10;
+$y = 28;
+
+// Method 1
+echo " This is 1st method.";
+echo "<br>Value of X is ".$x;
+echo "<br> Value of Y is ".$y;
+
+// Method 2
+echo "<br><br> This is 2nd method.";
+echo "<br> Value of X is".$x."<br> Value of Y is".$y;
+
+// Method 3
+echo "<br><br> This is 3rd method.";
+echo "<br> The value of X is $x and the value of Y is $y";
+
+// Method 4
+echo "<br><br> This is 4th Method.";
+echo "<br> The value of X is $x and the value of Y is $y and their sum is ".($x+$y);
+
+?>
